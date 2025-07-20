0899bb76a1909d3f0cf9c03c2f1de58cfa09ad99
Cedric Dousset <cedric@mac.home>
2025-07-20 15:14:29 +0200

    Fix CSS build error by disabling minification

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  // Disable all CSS optimization to fix build error
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
+      // Disable all CSS minification
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig 
